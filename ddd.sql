ac766f5246ff18f05253d840ccc970fd9ae18383
wuchinshu <wuchinshu@yahoo.com.tw>
2017-03-28 15:51:10 +0800

    Add existing file

--- ddd.sql
+++ ddd.sql
@@ -1,3 +1,4 @@
-insert into ec_order_temp (rowdata) values ('109-5212051-0177037	44486579645706	2017-03-18T13:23:23-07:00	2017-03-18T13:23:23-07:00	8j217bxf86w5h41@marketplace.amazon.com	Mark St. Germaine	7155800935	EG-1PBS	360 Video Camera LyfieEye : 360 Degree Dual Lens HD Video Camera for Android Smartphone (Polished Black Full Set)	1	USD	129.00	0.00	6.85	0.00	Standard	Mark St Germaine	1040 27TH AVE SE APT F			MINNEAPOLIS	MN	55414-2734	US	7155800935				');
-commit;
-begin EC_LOAD_AMAZON; end;
+test0001
+
+
+SQL
--- test001.c
+++ test001.c
@@ -1,1 +1,3 @@
 #ccccccc
+test c 
+test b
